4b7b6db4638a0a1f8e39b83a1f5cdd42f1bc6114
Damian Wisniewski <damian.wisniewski@performgroup.com>
2019-12-13 10:48:47 +0100

    some refactorization :)

--- git.js
+++ git.js
@@ -3,51 +3,61 @@ const { execSync } = require('child_process');
 const { EOL } = require('os');
 const chalk = require('chalk');
 
-function pushBranchAndTags() {
-  const branchName = execSync('git branch | grep \\* | cut -d \' \' -f2').toString().trim()
-  console.log(`${EOL}${chalk.bgBlue.white(`Pushing branch ${branchName}...`)}${EOL}`);
+/**
+ * Returns branch name you are currently on
+ * @returns {string}
+ */
+function getCurrentBranchName() {
+  return execSync('git branch | grep \\* | cut -d \' \' -f2').toString().trim();
+}
+
+/**
+ * Pushes tags and branch, you are currently on
+ */
+function pushCurrentBranchAndTags() {
+  console.log(chalk.blue.bold(`Pushing branch ${getCurrentBranchName()}...`));
 
   console.log(execSync('git push').toString());
   console.log(execSync('git push --tags').toString());
 }
 
-function rebaseDevelopWithMaster() {
+/**
+ * Rebasing branches
+ * @param {string} branchToRebase branch to checkout on
+ * @param {string} rebaseTarget branch to rebase with
+ */
+function rebaseBranches(branchToRebase, rebaseTarget) {
   try {
-    console.log(`${EOL}${chalk.bgBlue.white(`Rebasing branch develop onto master...`)}${EOL}`);
-    console.log(execSync('git rebase master develop').toString());
+    console.log(chalk.blue.bold(`Rebasing branch ${branchToRebase} onto ${rebaseTarget}...`));
+    console.log(execSync(`git rebase ${rebaseTarget} ${branchToRebase}`).toString());
   } catch (err) {
     const processOutput = err.stdout.toString();
     const errorInfo = err.stderr.toString();
 
-    console.log(`${EOL}run: "git rebase master develop"${EOL}`);
-
     console.log(processOutput);
-    console.log(chalk.bgRed.white('!!!  WATCH OUT  !!!'));
-    console.log('You are trying to rebase develop onto master');
-    console.log(`${EOL}${chalk.bgRed.white(errorInfo)}`);
+    console.log(chalk.bgRed.white('!!!  ERROR  !!!'));
+    console.log(`${EOL}${chalk.red(errorInfo)}`);
 
     if (processOutput.search('CONFLICT') > -1) {
       console.log(chalk.red('Some CONFLICTS were found!'));
       execSync('git rebase --abort');
-      console.log(`Rebase was aborted. Please rebase it manually!${EOL}`);
+      console.log(`Rebase was aborted. Please rebase it manually!`);
     }
 
     process.exit(1);
   }
 }
 
-function updateGitOrigin(branchName) {
-  if (branchName === 'develop') {
-    pushBranchAndTags();
-  } else if (branchName === 'master') {
-    pushBranchAndTags();
-    rebaseDevelopWithMaster();
-    pushBranchAndTags();
-  } else {
-    console.error(`${EOL}${chalk.bgRed.white(`Something is wrong, you are on ${branchName} branch!`)}`);
-    console.error(`${chalk.bgRed.white('Automatic update of git origin is available on develop or master only!')}`);
-    process.exit(1);
-  }
-}
+const branchName = getCurrentBranchName();
 
-updateGitOrigin(execSync('git branch | grep \\* | cut -d \' \' -f2').toString().trim());
+if (branchName === 'develop') {
+  pushCurrentBranchAndTags();
+} else if (branchName === 'master') {
+  pushCurrentBranchAndTags();
+  rebaseBranches('develop', 'master');
+  pushCurrentBranchAndTags();
+} else {
+  console.error(`${EOL}${chalk.bgRed.white(`Something is wrong, you are on ${branchName} branch!`)}`);
+  console.error(`${chalk.bgRed.white('Automatic update of git origin is available on develop or master only!')}`);
+  process.exit(1);
+}
